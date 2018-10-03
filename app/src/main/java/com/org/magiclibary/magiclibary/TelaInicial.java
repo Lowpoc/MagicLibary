fe6f69c49e2195256bd111c2aab07eaeafe26870
Marcus Vinicius Santana Silva <marcus.vinicius.3007@gmail.com>
2018-10-03 00:58:56 -0300

    Criando layout com grid view

--- app/src/main/java/com/org/magiclibary/magiclibary/TelaInicial.java
+++ app/src/main/java/com/org/magiclibary/magiclibary/TelaInicial.java
@@ -2,6 +2,8 @@ package com.org.magiclibary.magiclibary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class TelaInicial extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+
+    public void showCards(View view) {
+        Toast.makeText(this.getApplicationContext(), "Cards", Toast.LENGTH_LONG).show();
+    }
+
+    public void showSets(View view) {
+        Toast.makeText(this.getApplicationContext(), "Sets", Toast.LENGTH_LONG).show();
     }
 }
