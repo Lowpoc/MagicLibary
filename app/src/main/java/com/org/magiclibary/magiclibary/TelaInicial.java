82b5bd630aaf0f052a42d01ff993799f95787500
Marcus Vinicius Santana Silva <marcus.vinicius.3007@gmail.com>
2018-11-03 13:28:09 -0300

    Intent implicito

--- app/src/main/java/com/org/magiclibary/magiclibary/TelaInicial.java
+++ app/src/main/java/com/org/magiclibary/magiclibary/TelaInicial.java
@@ -3,15 +3,14 @@ package com.org.magiclibary.magiclibary;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -45,5 +44,9 @@ public class TelaInicial extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         finish();
         return true;
+    }
+
+    public void showSite(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://magic.wizards.com/pt-br")));
     }
 }
